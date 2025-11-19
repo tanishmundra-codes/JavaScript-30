8b700485912239a8590bd4fb9514e506184a4c2d
tanishmundra-codes <tanishmundra2@gmail.com>
2025-11-19 22:53:15 +0530

    updated index file

--- Day 5/practical/index.js
+++ Day 5/practical/index.js
@@ -1,3 +1,6 @@
+// Build a simple "Search Filter." Use your fruits list from yesterday. 
+// Add an <input>. As the user types, filter the list to only show matching fruits.
+
 let searchInput = document.querySelector("#Search");
 let fruitsList = document.querySelector("#fruits-list");
 
