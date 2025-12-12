86eba1dc31091b278fdda2e1158dfa6672232984
tanishmundra-codes <tanishmundra2@gmail.com>
2025-12-12 22:18:07 +0530

    changes code

--- Day 5/practical/index.js
+++ Day 5/practical/index.js
@@ -7,19 +7,15 @@ let fruitsList = document.querySelector("#fruits-list");
 let listItemNode = fruitsList.querySelectorAll("li"); 
 
 searchInput.addEventListener('input', () => {
-    // Get the search term and convert it to lowercase once.
+
     const searchTerm = searchInput.value.toLowerCase();
 
-    // Iterate directly over the HTML <li> elements.
     listItemNode.forEach((li) => {
-        const fruitName = li.textContent.toLowerCase();
-        
-        // Check if the fruit name includes the search term.
+        const fruitName = li.textContent.toLowerCase();  
+      
         if (fruitName.includes(searchTerm)) {
-            // Show the element if it matches
             li.style.display = ''; 
         } else {
-            // Hide the element if it doesn't match
             li.style.display = 'none'; 
         }
     });
